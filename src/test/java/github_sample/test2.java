fcc7a6c847b6fd65529c5a168d557587f6575064
dell <dell@krish>
2023-04-04 11:01:44 +0530

    sprint1

---
+++ src/test/java/github_sample/test2.java
@@ -0,0 +1,20 @@
+package github_sample;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	
+	
+	@Test
+	public void login()
+	{
+		System.out.println("login");
+	}
+	
+	public void logout()
+	{
+		System.out.println("logout");
+	}
+}
+
+
